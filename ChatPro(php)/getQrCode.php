cca1cec0a996abf2aef4b70461901a9dadec84f5
wellmtx <92235653+wellmtx@users.noreply.github.com>
2021-12-22 10:04:12 -0300

    Update getQrCode.php

--- ChatPro(php)/getQrCode.php
+++ ChatPro(php)/getQrCode.php
@@ -3,11 +3,11 @@
 $ch = curl_init();
 
 curl_setopt_array($ch, array(
-	CURLOPT_URL => 'https://v4.chatpro.com.br/chatpro-qpebpshq56/api/v1/generate_qrcode?webhook=https%3A%2F%2Fwebhook.site%2Fc08c37b6-a2e1-4dd9-8ef2-d1d2ce0889d2' ,
+	CURLOPT_URL => 'https://v4.chatpro.com.br/[endPoint]/api/v1/generate_qrcode?webhook=https%3A%2F%2Fwebhook.site%2Fc08c37b6-a2e1-4dd9-8ef2-d1d2ce0889d2' ,
 	CURLOPT_CUSTOMREQUEST => 'GET' ,
 	CURLOPT_RETURNTRANSFER => true ,
 	CURLOPT_HTTPHEADER => array(
-		"Authorization: eop82iy1mdf4ohbez2wlmea7n21y4d" ,
+		"Authorization: [seuToken]" ,
 		"cache_control: no-cache"
 	)
 ));
