84de5dd54e0d6f6137e9772ee43edd130c4637fe
wellmtx <92235653+wellmtx@users.noreply.github.com>
2021-12-22 10:12:26 -0300

    Update getStatus.py

--- ChatPro(py)/getStatus.py
+++ ChatPro(py)/getStatus.py
@@ -1,9 +1,9 @@
 import requests
 
-url = 'https://v4.chatpro.com.br/chatpro-qpebpshq56/api/v1/status'
+url = 'https://v4.chatpro.com.br/[EndPoint]/api/v1/status'
 
 headers = {
-  'Authorization': "eop82iy1mdf4ohbez2wlmea7n21y4d" ,
+  'Authorization': "[seuToken]" ,
   'cache-control': "no-cache"
 }
 
