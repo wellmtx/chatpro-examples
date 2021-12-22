56b7daf4e7c3cff945772a9db17f8fd02198dc89
wellmtx <92235653+wellmtx@users.noreply.github.com>
2021-12-22 10:15:02 -0300

    Update sendMessage.py

--- ChatPro(py)/sendMessage.py
+++ ChatPro(py)/sendMessage.py
@@ -3,12 +3,12 @@ sendToNumber = str(input('Número do destinatário: '))
 
 textMessage = str(input('Escreva sua mensagem: '))
 
-url = 'https://v4.chatpro.com.br/chatpro-qpebpshq56/api/v1/send_message'
+url = 'https://v4.chatpro.com.br/[EndPoint]/api/v1/send_message'
 
 messageLoad = '{ "menssage": "' + textMessage + '" , "number": "' + sendToNumber + '" }'
 
 headers = {
-  'Authorization': "eop82iy1mdf4ohbez2wlmea7n21y4d" ,
+  'Authorization': "[seuToken]" ,
   'cache-control': "no-cache"
 }
 
