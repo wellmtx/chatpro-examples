a1c89b9e6572272442efef5eebce25ddfd4d0fb0
wellmtx <92235653+wellmtx@users.noreply.github.com>
2021-12-22 10:10:52 -0300

    Update QRCode.py

--- ChatPro(py)/QRCode.py
+++ ChatPro(py)/QRCode.py
@@ -2,10 +2,10 @@ from PIL import Image
 import requests
 from io import BytesIO
 
-url = 'https://v4.chatpro.com.br/chatpro-qpebpshq56/api/v1/generate_qrcode?webhook=https%3A%2F%2Fwebhook.site%2Fc08c37b6-a2e1-4dd9-8ef2-d1d2ce0889d2'
+url = 'https://v4.chatpro.com.br/[EndPoint]/api/v1/generate_qrcode'
 
 headers = {
-  'Authorization': "eop82iy1mdf4ohbez2wlmea7n21y4d" ,
+  'Authorization': "[seuToken]" ,
   'cache-control': "no-cache"
 }
 
