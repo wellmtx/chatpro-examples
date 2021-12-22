49a55744321985f95b840a50e9a49ff90ba5b59c
wellmtx <92235653+wellmtx@users.noreply.github.com>
2021-12-22 10:05:22 -0300

    Update sendFiles.php

--- ChatPro(php)/sendFiles.php
+++ ChatPro(php)/sendFiles.php
@@ -7,12 +7,12 @@ $urlFile = $_POST['contentUrl'];
 $ch = curl_init();
 
 curl_setopt_array($ch, array(
-	CURLOPT_URL => 'https://v4.chatpro.com.br/chatpro-qpebpshq56/api/v1/send_message_file_from_url' ,
+	CURLOPT_URL => 'https://v4.chatpro.com.br/[EndPoint]/api/v1/send_message_file_from_url' ,
 	CURLOPT_RETURNTRANSFER => true ,
 	CURLOPT_CUSTOMREQUEST => "POST" ,
 	CURLOPT_POSTFIELDS => "{ \"caption\": \"$nameFile\", \"number\": \"$number\", \"url\": \"$urlFile\"}" ,
 	CURLOPT_HTTPHEADER => array (
-		"Authorization: eop82iy1mdf4ohbez2wlmea7n21y4d",
+		"Authorization: [seuToken]",
 		"cache_control: no-cache"
 	))
 );
