5b8b8f248745ed3d7233eb900fc7f71eca009471
wellmtx <92235653+wellmtx@users.noreply.github.com>
2022-01-11 12:15:21 -0300

    Update reload.php

--- ChatPro(php)/reload.php
+++ ChatPro(php)/reload.php
@@ -5,7 +5,7 @@ require_once("class/requestCurl.php");
 function status() 
 {
     $getStatus = new requestCurl();
-    $getStatus->setUrl("https://v4.chatpro.com.br/chatpro-xbcsytl3hj/api/v1/status");
+    $getStatus->setUrl("https://v4.chatpro.com.br/[EndPoint]/api/v1/status");
     $getStatus->setMethod("GET");
     $statusConnection = $getStatus->request();
     return $statusConnection["connected"];
@@ -14,7 +14,7 @@ function status()
 function reload() 
 {
     $reload = new requestCurl();
-    $reload->setUrl("https://v4.chatpro.com.br/chatpro-xbcsytl3hj/api/v1/reload");
+    $reload->setUrl("https://v4.chatpro.com.br/[EndPoint]/api/v1/reload");
     $reload->setMethod("GET");
     $reload->request();
 
