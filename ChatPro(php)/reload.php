6fd6122fb8f6f504dc39940029cff51cf81b8dfa
wellmtx <92235653+wellmtx@users.noreply.github.com>
2022-01-11 12:14:33 -0300

    Add files via upload

---
+++ ChatPro(php)/reload.php
@@ -0,0 +1,26 @@
+<?php 
+
+require_once("class/requestCurl.php");
+
+function status() 
+{
+    $getStatus = new requestCurl();
+    $getStatus->setUrl("https://v4.chatpro.com.br/chatpro-xbcsytl3hj/api/v1/status");
+    $getStatus->setMethod("GET");
+    $statusConnection = $getStatus->request();
+    return $statusConnection["connected"];
+}
+
+function reload() 
+{
+    $reload = new requestCurl();
+    $reload->setUrl("https://v4.chatpro.com.br/chatpro-xbcsytl3hj/api/v1/reload");
+    $reload->setMethod("GET");
+    $reload->request();
+
+    echo "Reconectando";
+}
+
+status() === true ? "Você já está conectado" : reload();
+
+?>
