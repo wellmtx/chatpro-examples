bafadd3abc914b2dbf3e6188d46a07db93cc181e
Wellington Saraiva <wellingtonsaraivasousa@gmail.com>
2022-10-07 10:07:56 -0300

    Update getStatus.php

--- ChatPro(php)/getStatus.php
+++ ChatPro(php)/getStatus.php
@@ -3,7 +3,7 @@
 $ch = curl_init();
 
 curl_setopt_array($ch, array(
-	CURLOPT_URL => "https://v4.chatpro.com.br/[EndPoint]/api/v1/status" ,
+	CURLOPT_URL => "https://v5.chatpro.com.br/[EndPoint]/api/v1/status" ,
 	CURLOPT_CUSTOMREQUEST => "GET" ,
 	CURLOPT_RETURNTRANSFER => true ,
 	CURLOPT_HTTPHEADER => array(
