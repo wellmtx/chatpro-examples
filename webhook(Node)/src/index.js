a3dbd5b95fb155d49ab2e212e26519ee4af566d7
wellmtx <wellingtonsaraivasousa@gmail.com>
2023-07-27 13:07:30 -0300

    fix: :fire: remove import

--- webhook(Node)/src/index.js
+++ webhook(Node)/src/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
 app.post("/webhook", (request, response) => {
   response.send();
